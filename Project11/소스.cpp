c3959d59b47d2b888e20cf9b276223aa09215699
수현 이 <7553321@naver.com>
2019-11-15 11:00:05 +0900

    초기 커밋.

---
+++ Project11/소스.cpp
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+
+int main() {
+	
+	int n = 5;
+	int i,j;
+	for (i = n;i > 0;i--)
+	{
+		for (j = i;j > 0;j--)
+			printf("%d", j);
+		printf("\n");
+	}
+
+	
+
+
+
+
+
+
+}
